b6dc06f04ed4411a04df075de76409896ed58f13
C00261577 <148890591+C00261577@users.noreply.github.com>
2023-12-01 03:24:48 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
-
 const key ='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN0c3RxaHZiZWRxdHpuZXp2bHVmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDEyMjE1NjAsImV4cCI6MjAxNjc5NzU2MH0.kWV1RY12sppWe8jibUEBs6f9_EiF4jbUCaApGL8BdaI'
+
 const customDomain ='https://ctstqhvbedqtznezvluf.supabase.co'
-import { createClient} from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+
+
+
+
+// Initialize Supabase client with your project URL and API key
+const supabase = createClient('https://ctstqhvbedqtznezvluf.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN0c3RxaHZiZWRxdHpuZXp2bHVmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDEyMjE1NjAsImV4cCI6MjAxNjc5NzU2MH0.kWV1RY12sppWe8jibUEBs6f9_EiF4jbUCaApGL8BdaI');
 
 document.addEventListener('DOMContentLoaded', () => {
-  // You can perform some initialization here if needed
+  // Load existing notes on page load
+  showNotes();
 });
 
 async function addNote() {
@@ -13,25 +19,24 @@ async function addNote() {
 
   if (noteText !== '') {
     // Insert a new note into the 'notes' table
-    const { data, error } = await supabase.from('notes').insert([{ text: noteText }]);
+    await supabase.from('notes').insert([{ text: noteText }]);
+    
+    // Show all notes including the newly added one
+    showNotes();
 
-    if (error) {
-      console.error('Error adding note:', error.message);
-    } else {
-      // Show all notes including the newly added one
-      showNotes();
-
-      noteInput.value = ''; // Clear the input field
-
-      // Display a message
-      const messageElement = document.getElementById('message');
-      messageElement.innerText = 'Note added successfully!';
-      setTimeout(() => {
-        messageElement.innerText = '';
-      }, 3000); // Clear the message after 3 seconds
-    }
+    noteInput.value = ''; // Clear the input field
   }
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  // Check the current page and execute the corresponding function
+  const currentPage = window.location.pathname;
+  if (currentPage.includes('show_notes.html')) {
+    showNotes();
+  } else if (currentPage.includes('delete_notes.html')) {
+    deleteAllNotes();
+  }
+});
 
 async function showNotes() {
   // Retrieve all notes from the 'notes' table
@@ -45,26 +50,14 @@ async function showNotes() {
   }
 }
 
-function displayNotes(notes) {
+async function deleteAllNotes() {
+  // Delete all notes from the 'notes' table
+  await supabase.from('notes').delete();
+
+  // Clear the notes container
   const notesContainer = document.getElementById('notes-container');
-  notesContainer.innerHTML = ''; // Clear previous notes
-
-  notes.forEach((note) => {
-    const noteElement = document.createElement('div');
-    noteElement.classList.add('note');
-    noteElement.innerHTML = `
-      <p>${note.text}</p>
-      <button onclick="deleteNote(${note.id})">Delete</button>
-    `;
-
-    notesContainer.appendChild(noteElement);
-  });
+  notesContainer.innerHTML = 'All notes deleted.';
 }
 
-async function deleteNote(noteId) {
-  // Delete a specific note by ID
-  await supabase.from('notes').delete().eq('id', noteId);
+// ... (other functions remain unchanged)
 
-  // Show the updated list of notes
-  showNotes();
-}
